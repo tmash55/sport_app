c0f9e033db0b25a0e7004fd691144351dc8adf49
TylerMaschoff <66757633+Tymash55@users.noreply.github.com>
2025-03-07 14:12:27 -0600

    update domain

--- config.ts
+++ config.ts
@@ -7,7 +7,7 @@ const config = {
   appDescription:
     "Dryft delivers innovative and engaging sports pools, redefining the way fans compete with dynamic, strategy-driven formats beyond traditional brackets.",
   // REQUIRED (no https://, not trialing slash at the end, just the naked domain)
-  domainName: "shipfa.st",
+  domainName: "dryftplay.com",
   crisp: {
     // Crisp website ID. IF YOU DON'T USE CRISP: just remove this => Then add a support email in this config file (resend.supportEmail) otherwise customer support won't work.
     id: "",
